b9f258cabd2e39dc0569fa47de02424a516c99ae
Luis Lavena <luislavena@gmail.com>
2008-11-03 23:35:15 -0300

    D'oh, all this time forgot to commit ExtensionTask itself.

---
+++ lib/rake/extensiontask.rb
@@ -0,0 +1,112 @@
+#!/usr/bin/env ruby
+
+# Define a series of tasks to aid in the compilation of C extensions for
+# gem developer/creators.
+
+require 'rake'
+require 'rake/tasklib'
+require 'rbconfig'
+
+module Rake
+  class ExtensionTask < TaskLib
+    attr_accessor :name
+    attr_accessor :config_script
+    attr_accessor :tmp_dir
+    attr_accessor :ext_dir
+    attr_accessor :lib_dir
+    attr_accessor :source_pattern
+
+    def initialize(name = nil)
+      init(name)
+      yield self if block_given?
+      define
+    end
+
+    def init(name = nil)
+      @name = name
+      @config_script = 'extconf.rb'
+      @tmp_dir = 'tmp'
+      @ext_dir = 'ext'
+      @lib_dir = 'lib'
+      @source_pattern = "*.c"
+    end
+
+    def define
+      fail "Extension name must be provided." if @name.nil?
+
+      # directories we need
+      directory "#{@tmp_dir}/#{@name}"
+      directory @lib_dir
+
+      # makefile depends of tmp_dir and config_script
+      # tmp/extension_name/Makefile
+      file makefile => [tmp_path, extconf] do
+        parent = Dir.pwd
+        Dir.chdir tmp_path do
+          ruby File.join(parent, extconf)
+        end
+      end
+
+      # binary in temporary folder depends on makefile and source files
+      # tmp/extension_name/extension_name.{so,bundle}
+      file tmp_binary => [makefile] + source_files do
+        Dir.chdir tmp_path do
+          sh make
+        end
+      end
+
+      # copy binary from temporary location to final lib
+      # tmp/extension_name/extension_name.{so,bundle} => lib/
+      file lib_binary => [lib_path, tmp_binary] do
+        cp tmp_binary, lib_binary
+      end
+
+      desc "Compile just the #{@name} extension"
+      task "compile:#{@name}" => [lib_binary]
+
+      desc "Compile the extension(s)"
+      task "compile" => ["compile:#{@name}"]
+    end
+
+    private
+    def makefile
+      "#{tmp_path}/Makefile"
+    end
+
+    def extconf
+      "#{ext_path}/#{@config_script}"
+    end
+
+    def tmp_path
+      File.join(@tmp_dir, @name)
+    end
+
+    def ext_path
+      File.join(@ext_dir, @name)
+    end
+
+    def lib_path
+      @lib_dir
+    end
+
+    def make
+      RUBY_PLATFORM =~ /mswin/ ? 'nmake' : 'make'
+    end
+
+    def binary
+      "#{@name}.#{RbConfig::CONFIG['DLEXT']}"
+    end
+
+    def source_files
+     @source_files ||= FileList["#{ext_path}/#{@source_pattern}"]
+    end
+
+    def tmp_binary
+      "#{tmp_path}/#{binary}"
+    end
+
+    def lib_binary
+      "#{lib_path}/#{binary}"
+    end
+  end
+end
