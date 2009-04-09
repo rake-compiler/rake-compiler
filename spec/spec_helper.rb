491d6a9237b1647eb1d47ba3a1b2c122431908c5
Luis Lavena <luislavena@gmail.com>
2009-04-09 20:33:56 -0300

    Added Console redirection helper
    The idea is be able to capture output from OUT and ERR in the
    specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,9 @@ $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 require 'rubygems'
 require 'spec'
 
+# Console redirection helper
+require File.expand_path(File.join(File.dirname(__FILE__), 'support/capture_output_helper'))
+
 Spec::Runner.configure do |config|
+  include CaptureOutputHelper
 end
