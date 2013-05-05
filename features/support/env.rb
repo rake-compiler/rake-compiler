5e0076a78fc173921c3067f1045295cb93abe397
Luis Lavena <luislavena@gmail.com>
2013-05-05 14:05:51 -0300

    Avoid Bundler poluting cross-compilation
    Since we started to use Bundler, it modified RUBYOPT environment
    variable which affected cross compilation scenarios.
    
    Unset RUBYOPT variable if Bundler is defined when running Cucumber.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,7 @@ require 'fileutils'
 require 'rbconfig'
 
 ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), '../..'))
+
+# get rid of Bundler environment polution
+defined?(Bundler) and
+  ENV.delete("RUBYOPT")
