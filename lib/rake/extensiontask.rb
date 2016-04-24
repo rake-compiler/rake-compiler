accce2997abe5d1888959d927949b184e4d1c1c1
Kouhei Sutou <kou@clear-code.com>
2016-04-24 21:37:19 +0900

    Simplify

--- lib/rake/extensiontask.rb
+++ lib/rake/extensiontask.rb
@@ -112,8 +112,7 @@ Rerun `rake` under MRI Ruby 1.8.x/1.9.x to cross/native compile.
 
       # target_prefix
       if @name.include?('/')
-        target_prefix = File.dirname(@name)
-        target_prefix[0,0] = '/'
+        target_prefix = "/#{File.dirname(@name)}"
       else
         target_prefix = ''
       end
